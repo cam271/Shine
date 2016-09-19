12eb66bdc3eaaaad024ed9902eca2dc91d62193e
Messenger, C <messenger_c@ymail.com>
2016-09-18 23:38:55 -0400

    added index to lower case email and username

--- db/structure.sql
+++ db/structure.sql
@@ -177,6 +177,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -210,6 +231,6 @@ CREATE UNIQUE INDEX index_users_on_reset_password_token ON users USING btree (re
 
 SET search_path TO "$user", public;
 
-INSERT INTO schema_migrations (version) VALUES ('20160915180854'), ('20160916151227'), ('20160916194151');
+INSERT INTO schema_migrations (version) VALUES ('20160915180854'), ('20160916151227'), ('20160916194151'), ('20160918033954');
 
 
