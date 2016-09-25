f70d3dbf61113244111add32466c73317e7ee976
Messenger, C <messenger_c@ymail.com>
2016-09-24 20:25:24 -0400

    acceptance test for the typehead search

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/poltergeist'
 
-Cpaybara.javascript_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
 Capybara.default_driver = :poltergeist
 
 # Add additional requires below this line. Rails is not loaded until this point!
